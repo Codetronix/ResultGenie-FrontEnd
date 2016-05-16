965580bdf5cba64847ce3e13398dd881f4bf7807
kingspp <kingspprathyush@gmail.com>
2016-05-16 21:57:29 +0530

    Initialized Template

--- js/script.js
+++ js/script.js
@@ -1,30 +1,7 @@
  $(document).ready(function() {
-
-//  $( "#disclaimer" ).hide();
-//  $( "#disclaimer" ).show( "fast", function() {
-//     // Animation complete.
-//   });
- $('#loginForm').submit(function(){
-            var url = "/rest/v1/user/login";
-            var data = {};
-            data.username=$('#username').val();
-            data.password=$('#password').val();
-            $.ajax({
-                   type: "POST",
-                   url: url,
-                   data: JSON.stringify(data),
-                   dataType: 'json',
-                   contentType: "application/json",
-                   success: function(data)
-                   {
-                     // localStorage.setItem("isSameSession",false);
-                      window.location.href="/index.html";
-                   },
-                   error:function(data){
-                        alert("Login failed");
-                        console.log(data);
-                   }
-                 });
-            return false;
-        });
-        });
+// $('.button-collapse').sideNav({
+//       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+//     }
+//   );
+//   $('.collapsible').collapsible();
+});
